185dcbca56b4b8c46cde74bd3388c7180045a696
Ciro Monteiro <cirossmonteiro@gmail.com>
2023-10-28 19:25:26 -0300

    db test

--- backend/db.ts
+++ backend/db.ts
@@ -1,8 +1,35 @@
 import dotenv from "dotenv";
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from "mongoose";
 
 dotenv.config();
 
+class DBInstance {
+  db?: MongoMemoryServer = undefined;
+  constructor() {
+    MongoMemoryServer.create().then(db => {
+      this.db = db;
+    });
+  }
+}
+
+function sleep(seconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds*1000);
+  });
+}
+
+const instance = new DBInstance();
+
 const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DBNAME}.mongodb.net/?retryWrites=true&w=majority`;
 
-export default () => mongoose.connect(URI);
+export default async (inMemory: boolean = true) => {
+  if (inMemory) {
+    while(!instance.db) {
+      await sleep(1);
+    }
+    await mongoose.connect(instance.db.getUri());
+  } else {
+    await mongoose.connect(URI);
+  }
+}
